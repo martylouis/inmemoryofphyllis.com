f490b2f0e6ebdc7a248f9bb56d89b7a0970a5bb8
Marty Thierry <marty@martylouis.com>
2021-08-23 16:20:04 -0500

    wip: update all
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,43 @@
 import Head from "next/head"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="min-h-screen text-gray-100 bg-[#2f3738]">
       <Head>
-        <title>Create Next App</title>
+        <title>In Memory of Phyllis Hartman</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div className="min-h-screen lg:relative">
+          <div className="relative w-full h-[56vh] lg:absolute lg:inset-y-0 lg:right-0 lg:w-3/5 lg:h-screen">
+            <Image
+              className="absolute inset-0 object-cover object-top w-full h-full"
+              src="/phartman--01.jpg"
+              alt="Picture of Phyllis Marie Hartman"
+              layout="fill"
+              priority="true"
+            />
+          </div>
+          <div className="flex items-center justify-center lg:justify-start w-full pt-16 pb-20 mx-auto text-center bg-[#2f3738] lg:h-screen">
+            <div className="px-4 lg:w-2/5 sm:px-8 xl:pr-16">
+              <header className="text-center">
+                <h1>
+                  <span className="block font-fancy text-[3rem] lg:text-[4rem] leading-none">
+                    In loving memory of
+                  </span>
+                  <span className="block">Phyllis Marie Hartman</span>
+                </h1>
+                <p>1932 — 2021</p>
+              </header>
+              <div>
+                <h2>Download Obituary</h2>
+                <h2>Watch Video Memorial Slideshow</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
