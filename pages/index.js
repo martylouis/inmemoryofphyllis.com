0eaabe60859780ee766cb31fb0d8cf553f672c64
Marty Thierry <marty@martylouis.com>
2021-08-28 12:16:41 -0400

    feat: update site favicons

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,24 @@ export default function Home() {
       <Head>
         <title>In Memory of Phyllis Hartman</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="relative min-h-screen">
         <section>
